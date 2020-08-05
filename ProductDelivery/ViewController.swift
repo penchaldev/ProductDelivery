e5367968f95aa430a7d376dfd11d173f5951ba25
Penchalreddy <penchal.vr@gmail.com>
2020-08-05 08:09:19 +0530

    Initial Commit

---
+++ ProductDelivery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProductDelivery
+//
+//  Created by Vijay on 05/08/20.
+//  Copyright © 2020 senix.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
